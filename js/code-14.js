62688c38dacc290eb193dc402d610fc996bb3396
jamyyu <103821947+jamyyu@users.noreply.github.com>
2024-07-15 11:47:17 +0800

    Update code-14.js

--- js/code-14.js
+++ js/code-14.js
@@ -1,7 +1,10 @@
-const user14 =document.querySelector("＃users>.user-14");
-user14.textContent="貓貓";
-const img = document.createElement('img');
-img.src = 'https://placekitten.com/300/200';
-img.style.width = '30px'; 
-img.style.hight = '30px'; 
-user14.appendChild(img);
+function cat(){
+    const user14 =document.querySelector("#users>.user-14");
+    user14.textContent="貓貓";
+    const img = document.createElement('img');
+    img.src = 'https://placekitten.com/300/200';
+    img.style.width = '30px'; 
+    img.style.hight = '30px'; 
+    user14.appendChild(img);
+}
+cat();
