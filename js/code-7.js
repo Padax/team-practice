889b3da332290c785789c464d0a043b5012dc9e2
minruei <157027780+minruei@users.noreply.github.com>
2026-09-21 12:19:46 +0900

    Update code-7.js

--- js/code-7.js
+++ js/code-7.js
@@ -1,3 +1,3 @@
-const btn = document.createElement("button");
-btn.textContent = "嗨";
-document.body.appendChild(btn);
+const btn7 = document.createElement("button");
+btn7.textContent = "嗨";
+document.querySelector(".user-7").appendChild(btn7);
